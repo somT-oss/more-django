47a89c426541afb3e848923c566016a8a6051fa3
somT-oss <somtochukwuuchegbu@gmail.com>
2024-09-18 05:47:26 +0100

    add: settings config

---
+++ saas/config/settings/local.py
@@ -0,0 +1,15 @@
+from .base import * 
+
+ALLOWED_HOST = []
+
+INSTALLED_APPS += []
+
+DATABASES = {
+    "default": {
+        "ENGINE": 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'HOST': '',
+        'PASSWORD': '',
+        'USER': ''
+    }
+}
