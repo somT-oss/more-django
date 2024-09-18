26b8abe9f4ee701d817cbb477c67f95f82b32b05
somT-oss <somtochukwuuchegbu@gmail.com>
2024-09-18 05:23:57 +0100

    add: local settings.py

---
+++ saas/settings/local.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = True
+
+INSTALLED_APPS += []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'HOST': '',
+        'USER': '',
+        'PASSWORD': ''
+     }
+}
